6d266c3840cb27ebd3adff1b16d7752023128643
Victor Del Razo <vdelrazo@omnitracs.com>
2021-01-30 18:33:24 -0600

    Bunny CRUD

--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,27 @@
-require("core-js/modules/es.array.join.js");
-
 var express = require("express");
 
-var serveStatic = require("serve-static");
+var compression = require("compression");
 
-var path = require("path");
+var history = require("connect-history-api-fallback");
 
+var staticMiddleWare = express.static("dist");
 var app = express();
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
-var port = process.env.PORT || 3003;
-app.listen(port);
+app.use(compression());
+app.use(staticMiddleWare);
+app.use(history());
+app.use(staticMiddleWare);
+app.use(express.static("dist"));
+app.get("*.js", function (req, res, next) {
+  req.url = req.url + ".gz";
+  res.set("Content-Encoding", "gzip");
+  next();
+});
+app.get("*.js", function (req, res, next) {
+  req.url = req.url + ".gz";
+  res.set("Content-Encoding", "gzip");
+  next();
+});
+app.set("port", process.env.PORT || 3333);
+app.listen(app.get("port"), function () {
+  return console.log("Server @", app.get("port"));
+});

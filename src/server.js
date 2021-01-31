7a89edcb92ec727d0c4b3fcdd5a39de732dc0050
Victor Del Razo <vdelrazo@omnitracs.com>
2021-01-30 18:03:55 -0600

    Bunny CRUD

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.use(serveStatic(__dirname + "/dist"));
 const port = process.env.PORT || 3003;
 app.listen(port);
+console.log('server started '+ port);
